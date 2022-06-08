aec62c43dc7381549ab10737ca7a11614c441368
MarceloCLopes <marc01mcl@gmail.com>
2022-06-08 16:21:45 -0300

    criação de action de listagem de adoção

--- src/main/java/br/com/mcl/adoteumpet/api/adocao/services/AdocaoService.java
+++ src/main/java/br/com/mcl/adoteumpet/api/adocao/services/AdocaoService.java
@@ -1,5 +1,7 @@
 package br.com.mcl.adoteumpet.api.adocao.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +20,15 @@ public class AdocaoService {
   private AdocaoMapper adocaoMapper;
 
   public AdocaoResponse create(AdocaoRequest adocaoRequest) {
-
     var adocaoToCreate = adocaoMapper.toModel(adocaoRequest);
     var createdAdocao = adocaoRepository.save(adocaoToCreate);
     return adocaoMapper.toResponse(createdAdocao);
+  }
+
+  public List<AdocaoResponse> findAll() {
+    return adocaoRepository.findAll()
+        .stream()
+        .map(adocaoMapper::toResponse)
+        .toList();
   }
 }
