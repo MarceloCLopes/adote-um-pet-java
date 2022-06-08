40e88868bc9875579a787ecedd0243154a53b92b
MarceloCLopes <marc01mcl@gmail.com>
2022-06-08 14:24:14 -0300

    adicionando validação

--- src/main/java/br/com/mcl/adoteumpet/api/adocao/dtos/AdocaoRequest.java
+++ src/main/java/br/com/mcl/adoteumpet/api/adocao/dtos/AdocaoRequest.java
@@ -2,6 +2,14 @@ package br.com.mcl.adoteumpet.api.adocao.dtos;
 
 import java.math.BigDecimal;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -15,7 +23,18 @@ import lombok.NoArgsConstructor;
 @JsonNaming(SnakeCaseStrategy.class)
 public class AdocaoRequest {
 
+  @Email
+  @NotNull
+  @NotEmpty
+  @Size(max = 255)
   private String email;
+
+  @NotNull
+  @Min(10)
+  @Max(100)
   private BigDecimal amount;
+
+  @NotNull
+  @Positive
   private Long petId;
 }
